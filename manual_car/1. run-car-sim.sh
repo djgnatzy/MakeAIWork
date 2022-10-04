8706b628716da933b25fef900c7e9cd04c43c5e2
djgnatzy <djgnatzy@gmail.com>
2022-10-04 13:12:14 +0200

    Manual Car - Ruud

---
+++ manual_car/1. run-car-sim.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export LIBGL_ALLOW_SOFTWARE=1
+python3 simpylc/simulations/car_manual_flex_track/world.py
---
+++ manual_car/2. run-manual-client.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd simpylc/simulations/car_manual_flex_track/control_client/
+python3 manual_client.py
